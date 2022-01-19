a3817e5e618a555d01400bbc0ef86a7109155b67
Rick James <rkj@fb.com>
2022-01-19 14:24:30 -0800

    score based on slot heuristic

---
+++ green_score.py
@@ -0,0 +1,54 @@
+import collections
+
+# score[index] -> dict[letter] -> count
+score = []
+guess_score = {}
+guesses = []
+solutions = []
+
+def parse():
+    data = open("guesses.txt", "r")
+    rlines = data.readlines()
+
+    for line in rlines:
+        line = line.strip()
+        guesses.append(line)
+
+    data = open("solutions.txt", "r")
+    rlines = data.readlines()
+
+    for line in rlines:
+        line = line.strip()
+        solutions.append(line)
+
+def analyze():
+    global score
+
+    for i in range(5):
+        score.append(collections.defaultdict(int))
+
+    for s in solutions:
+        for i in range(len(s)):
+            c = s[i]
+            score[i][c] += 1
+
+    for g in guesses:
+        g_score = 0
+        for i in range(len(g)):
+            c = g[i]
+            g_score += score[i][c]
+
+        guess_score[g] = g_score
+
+def dump():
+    sorted_dict = {k: v for k, v in sorted(guess_score.items(), key=lambda item: item[1])}
+    data = open("green_score.txt", "w")
+    for k,v in reversed(sorted_dict.items()):
+        data.write(k + " " + str(v) + '\n')
+
+def main():
+    parse()
+    analyze()
+    dump()
+
+main()
