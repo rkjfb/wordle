320ed750f409f11bba132fac36eae25d55733412
Rick James <rkj@fb.com>
2022-01-20 20:22:20 -0800

    position intersection score

--- position_intersection_score.py
+++ position_intersection_score.py
@@ -1,48 +1,48 @@
 # dumps the top+bottom N empty intersections, which have unique letters
 import collections
 
-empty = dict()
+intersection = dict()
+pos_score = dict()
 N = 5
 result = dict()
 
 def parse():
+    # count solutions
+    data = open("solutions.txt", "r")
+    rlines = data.readlines()
+    count_solutions = len(rlines)
+
+    # count intersections
     data = open("empty_intersection_count.txt", "r")
     rlines = data.readlines()
 
     for line in rlines:
         line = line.strip()
         s = line.split(" ")
-        empty[s[0]] = int(s[1])
+        intersection[s[0]] = count_solutions - int(s[1])
 
-# return True if s contains a double (or more) character
-def double(s):
-    for i in range(len(s)):
-        c = s[i]
-        if c in s[i+1:]:
-            return True
-            print("found: " + s)
-    return False
+    # position score
+    data = open("position_score.txt", "r")
+    rlines = data.readlines()
 
-def analyze(it):
-    count = 0
-    for k,v in it:
-        if double(k):
-            continue
-        result[k] = v
-        count += 1
-        if count >= N:
-            return
+    for line in rlines:
+        line = line.strip()
+        s = line.split(" ")
+        pos_score[s[0]] = int(s[1])
+
+def analyze():
+    for k,v in intersection.items():
+        result[k] = v + pos_score[k]
 
 def dump():
     sorted_dict = {k: v for k, v in sorted(result.items(), key=lambda item: item[1])}
-    data = open("unique_empty_minmax.txt", "w")
+    data = open("position_intersection_score.txt", "w")
     for k,v in reversed(sorted_dict.items()):
         data.write(k + " " + str(v) + '\n')
 
 def main():
     parse()
-    analyze(empty.items())
-    analyze(reversed(empty.items()))
+    analyze()
     dump()
 
 main()
